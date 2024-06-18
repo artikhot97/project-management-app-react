b8b8a8ba3c0819ba59de0aa175d46e0e16229356
artikhot97 <khotarti13@gmail.com>
2024-06-18 20:58:26 +0530

    Button comoponent created and reused

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSeclected";
 import ProjectSideBar from "./Components/ProjectSideBar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
